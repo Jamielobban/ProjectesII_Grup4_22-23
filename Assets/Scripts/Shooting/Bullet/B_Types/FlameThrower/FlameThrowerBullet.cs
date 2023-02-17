a4d6395f6a2e35ee2b7c075db10df0c704cb22c1
jamie <jamie.lobban@enti.cat>
2023-02-17 15:32:12 +0100

    WOMOMA

--- Assets/Scripts/Shooting/Bullet/B_Types/FlameThrower/FlameThrowerBullet.cs
+++ Assets/Scripts/Shooting/Bullet/B_Types/FlameThrower/FlameThrowerBullet.cs
@@ -7,46 +7,46 @@ using UnityEngine.ParticleSystemJobs;
 public class FlameThrowerBullet : Bullet
 {
     public bool isClicking;
-    private BlitController myblit;
+    //private BlitController myblit;
 
     protected override void Start()
     {
         timeShooted = Time.time;
         //playerpos = FindObjectOfType<PlayerMovement>();
-        myblit = FindObjectOfType<BlitController>();
+        //myblit = FindObjectOfType<BlitController>();
         bulletData.bulletDamage *= bulletData._damageMultiplier;
         bulletData.bulletRangeInMetres = FindObjectOfType<RightHand>().GetWeaponInHand().GetFireRate();
-        myblit._Fired = true;
+        //myblit._Fired = true;
         //bulletDamage = 0.5f * _damageMultiplier;
         //bulletRangeInMetres = FindObjectOfType<RightHand>().GetWeaponInHand().GetFireRate();
         //bulletSpeedMetresPerSec = 1;
         //bulletRadius = 0.23f;
         //Debug.Log("creatng bullet");
-        isClicking = true;
+        //isClicking = true;
 //rb = this.GetComponent<Rigidbody2D>();
 
     }
 
-    protected override void Update()
-    {
-        if (myblit._Fired)
-        {
-            isClicking = true;
-            myblit._Percentage += Time.deltaTime * 2;
-            myblit._Mat.SetFloat("_Percent", myblit._Percentage);
-            if (myblit._Percentage > 1)
-            {
-                myblit._Percentage = 0;
-               // myblit._Fired = false;
-            }
-            //myblit._Fired = true;
-        }
-        if (Input.GetMouseButtonUp(0)){
-            myblit._Fired = false;
-        }
-        //CinemachineShake.Instance.ShakeCamera(4f, .05f);
-        //base.Update();
-        //timeShooted = Time.time;        
+    protected override void Update() { 
+    //{
+    //   // if (myblit._Fired)
+    //    {
+    //        isClicking = true;
+    //        myblit._Percentage += Time.deltaTime * 2;
+    //        myblit._Mat.SetFloat("_Percent", myblit._Percentage);
+    //        if (myblit._Percentage > 1)
+    //        {
+    //            myblit._Percentage = 0;
+    //           // myblit._Fired = false;
+    //        }
+    //        //myblit._Fired = true;
+    //    }
+    //    //if (Input.GetMouseButtonUp(0)){
+    //        //myblit._Fired = false;
+    //    //}
+    //    //CinemachineShake.Instance.ShakeCamera(4f, .05f);
+    //    //base.Update();
+    //    //timeShooted = Time.time;        
 
     }
     public float GetFlamesDamage()
