97a163d72d0d3d8206dfa44cd90241f716c8eecd
Marc Calvet <marc.calvet@enti.cat>
2023-02-23 14:09:10 +0100

    Glow amph falta codi

---
+++ Assets/Scripts/ChangeTexture.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTexture : MonoBehaviour
+{
+    public Texture2D[] arrayTextures;
+    SpriteRenderer sr;
+    Material mat;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTexture(int arrayTexturePosition)
+    {
+        sr.material.SetTexture("_GlowTex", arrayTextures[arrayTexturePosition]);
+        Debug.Log(arrayTexturePosition);
+    }
+}
