2473b708e4ab4c1535ba21d241f14c824f629a92
Marc Calvet <marc.calvet@enti.cat>
2023-03-04 19:43:06 +0100

    anim

---
+++ Assets/Animation/Effects&Explosions/Impacts/Shotgun/ShotgunImpact.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunImpact : MonoBehaviour
+{
+    [SerializeField]
+    float timeActive;
+    float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - startTime >= timeActive)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
