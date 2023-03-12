d0c4ffb2b1aa331196144416e06528c8f376bf43
Marc Calvet <marc.calvet@enti.cat>
2023-03-12 13:45:04 +0100

    musica menu principal

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public AudioClip menuTheme;
+    void Start()
+    {
+        AudioManager.Instance.LoadSound(menuTheme, GameObject.FindGameObjectWithTag("MainCamera").transform, 0, true, false, 0.7f);
+        Time.timeScale = 1;
+    }
+
+    
+}
